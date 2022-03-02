057a6a71041e56945ed418c94a5ef79a63c877db
Vladislav Mikheev <slav009@yandex.ru>
2022-03-02 16:57:11 +0300

    Add some comments and simplification

--- RomeNumber.cpp
+++ RomeNumber.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-int FindOnes(string& s, const vector<char>& simbols){
-    int ind = s.size()-1;
-    while (ind >=0 && (simbols[0] == s[ind] ||
+int FindDigit(string& s, const vector<char>& simbols){
+    int ind = s.size() - 1;
+    while (ind >= 0 && (simbols[0] == s[ind] ||
                         simbols[1] == s[ind] ||
                         simbols[2] == s[ind])){
         --ind;
@@ -18,19 +18,17 @@ int FindOnes(string& s, const vector<char>& simbols){
     }
     if (ind == s.size()) return 0;
     string num = s.substr(ind);
-    s.resize(ind);
+    s.resize(ind);       // warning: cutting input string
     vector<string> nums(10, ""s);
     for (int i = 1; i < 10; ++i){
-        if (i < 4){
-            nums[i] = nums[i-1] + simbols[0];
-        } else if (i == 4) {
-            nums[i] = nums[0] + simbols[0] + simbols[1];
+        if (i == 4) { 
+            nums[i] = ""s + simbols[0] + simbols[1];
         } else if (i == 5) {
             nums[i] = simbols[1];
-        } else if (i < 9){
-            nums[i] = nums[i-1] + simbols[0];
         } else if (i == 9){
-            nums[i] = nums[0] + simbols[0] + simbols[2];
+            nums[i] = ""s + simbols[0] + simbols[2];
+        } else {
+            nums[i] = nums[i-1] + simbols[0];
         }
     }
     for (int i = 1; i < 10; ++i){
@@ -42,20 +40,22 @@ int FindOnes(string& s, const vector<char>& simbols){
 }
 
 int RomeNum(string& s){
-    vector<char> simbols_1 = {'I','V','X'};
-    vector<char> simbols_2 = {'X','L','C'};
-    vector<char> simbols_3 = {'C','D','M'};
+    vector<vector<char>> simbols ={
+        {'C','D','M'},
+        {'X','L','C'},
+        {'I','V','X'}
+    };                 // reverse order for next cycle
     vector<int> num(4);
-    num[3] = FindOnes(s, simbols_1);
-    num[2] = FindOnes(s, simbols_2);
-    num[1] = FindOnes(s, simbols_3);
+    for (int i = 3; i > 0; --i){
+        num[i] = FindDigit(s, simbols[i-1]);
+    }
     if (s == ""s || s == "M"s || s == "MM"s || s == "MMM"s) {
         num[0] = s.size();
     } else return -1;
     int ans = 0;
     for (int i = 0; i < 4; ++i){
         if (num[i] == -1) return -1;
-        ans = ans*10 + num[i];
+        ans = ans * 10 + num[i];
     }
     return ans;
 }
